2ae1ebc14fbbffcb444552403295059268c86df2
Abhishek Bhatia <bhatiaabhishek8893@gmail.com>
2025-11-27 15:20:20 +0530

    feat: Modularize all agents workflow poc (#16)

---
+++ chatbot/chatbot/agent_definitions/__init__.py
@@ -0,0 +1,11 @@
+from chatbot.agent_definitions.router import RouterAgent
+from chatbot.agent_definitions.shopping_actions import ShoppingActionsAgent
+from chatbot.agent_definitions.customer_service import CustomerServiceAgent
+from chatbot.agent_definitions.product_search import ProductSearchAgent
+
+__all__ = [
+    "RouterAgent",
+    "ShoppingActionsAgent",
+    "CustomerServiceAgent",
+    "ProductSearchAgent"
+]
