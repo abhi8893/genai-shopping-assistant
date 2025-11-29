380a882e660b1b855f7d02b4363aeaca3267e636
Abhishek Bhatia <bhatiaabhishek8893@gmail.com>
2025-11-29 14:25:16 +0530

    define cart models + add dummy models for other domains

---
+++ ecom-backend/domains/carts/models.py
@@ -0,0 +1,30 @@
+from core.database import Base
+from sqlalchemy import Column, Integer, Float, DateTime, ForeignKey
+
+__all__ = ("Cart", "CartItem")
+
+class Cart(Base):
+
+    __tablename__ = "Cart"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, nullable=False)
+    amount = Column(Float, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+
+
+
+class CartItem(Base):
+
+    __tablename__ = "CartItem"
+
+    id = Column(Integer, primary_key=True)
+    cart_id = Column(Integer, nullable=False)
+    product_id = Column(Integer, nullable=False)
+    # cart_id = Column(Integer, ForeignKey("Cart.id"), nullable=False)
+    # product_id = Column(Integer, ForeignKey("Product.id"), nullable=False)
+    quantity = Column(Float, nullable=False)
+    amount = Column(Float, nullable=False)
+
+
+    
+
