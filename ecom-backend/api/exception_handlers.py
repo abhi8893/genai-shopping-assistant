f2605262d77afbb82308ecb7a10778e47135c58d
Abhishek Bhatia <bhatiaabhishek8893@gmail.com>
2025-12-01 16:09:24 +0530

    add base exception handling in app

---
+++ ecom-backend/api/exception_handlers.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from exceptions import BaseAppException
+
+def app_exception_handler(request: Request, exc: BaseAppException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "success": False,
+            "detail": str(exc),
+            "error_code": "APPLICATION_ERROR"
+        }
+    )
+
+
+def register_exception_handlers(app: FastAPI):
+    app.exception_handler(Exception)(app_exception_handler)
---
+++ ecom-backend/exceptions.py
@@ -0,0 +1,19 @@
+from fastapi import status
+
+class BaseAppException(Exception):
+    
+    def __init__(self, message: str, status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR):
+        self.message = message
+        self.status_code = status_code
+        
+    def __str__(self):
+        return self.message
+
+
+class ResourceNotFoundException(BaseAppException):
+    def __init__(self, message: str):
+        super().__init__(message, status.HTTP_404_NOT_FOUND)
+
+class UnauthorizedException(BaseAppException):
+    def __init__(self, message: str):
+        super().__init__(message, status.HTTP_401_UNAUTHORIZED)
