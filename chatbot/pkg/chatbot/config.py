3b271d784d8afb8434258220f7b88725c10ccaae
Abhishek Bhatia <bhatiaabhishek8893@gmail.com>
2026-01-24 22:40:41 +0530

    Enable local docker dev for chat service with mounted volumes (#70)

--- chatbot/pkg/chatbot/config.py
+++ chatbot/pkg/chatbot/config.py
@@ -2,6 +2,11 @@ from chatbot.constants import PROJECT_ROOT
 import yaml
 from pathlib import Path
 
+# TODO: 
+# Make a distinction between installation package and framework
+# The config path specification should be part of the project / folder structure
+# created with the framework capabilities of the package but NOT part of the package itself
+# So something like chatbot create project <project_name> <project_dir> <args>
 CONFIG_FILE_PATH =  Path(PROJECT_ROOT) / "conf/config.yml"
 
 DOWNSTREAM_ROUTES = [
