9bd4eb0e8e354d638e6932eb910e8d409e55775b
Abhishek Bhatia <bhatiaabhishek8893@gmail.com>
2026-01-26 18:14:38 +0530

    add langfuse callback in langraph graph

--- chatbot/pkg/chatbot/graph/graph.py
+++ chatbot/pkg/chatbot/graph/graph.py
@@ -15,6 +15,7 @@ def build_graph(
         
     graph_builder = StateGraph(State)
 
+    # BUG: Conditional node doesn't show up in langfuse
     graph_builder.add_conditional_edges(START, router_agent.run)
 
     graph_builder.add_node("shopping_actions", shopping_actions_agent.run)
