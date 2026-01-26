bcca87421cd7a8cac8d31344dd00baf78b3b596f
Abhishek Bhatia <bhatiaabhishek8893@gmail.com>
2026-01-26 19:16:53 +0530

    fix: unresolved placeholder in RouterAgent

--- chatbot/pkg/chatbot/agent_definitions/router.py
+++ chatbot/pkg/chatbot/agent_definitions/router.py
@@ -15,15 +15,41 @@ class RouterAgent:
         self.config = config
         self.openai_client = openai.OpenAI() if openai_client is None else openai_client
 
+        self.system_prompt = self._get_system_prompt()
+
     @staticmethod
     def _validate_config(config):
         downstream_routes_in_config = [d["name"] for d in config['downstream_routes']]
         assert set(downstream_routes_in_config) == set(module_config.DOWNSTREAM_ROUTES), "Downstream routes in package config do not match downstream routes in the passed config"
 
 
+    @staticmethod
+    def _get_downstream_routes_description_for_prompt(downstream_routes):
+        routes_lst = [d['name'] for d in downstream_routes]
+        desc = f"The available downstream routes are: {routes_lst}"
+        desc += "\n\n"
+
+        for idx, route in enumerate(downstream_routes):
+            desc += f"\n(Route {idx+1}) `{route['name']}`: {route['description']}"
+            desc += "\nExamples:"
+            for idx, example in enumerate(route['examples']):
+                desc += f"\n{idx+1}: {example}"
+
+            desc += "\n\n"
+        
+        desc += '\n\n'
+        return desc
+
+    def _get_system_prompt(self):
+
+        downstream_routes_desc = self._get_downstream_routes_description_for_prompt(self.config['downstream_routes'])
+        system_prompt = self.config['prompts']['system_prompt'].format(downstream_routes_desc=downstream_routes_desc)
+        return system_prompt
+
+
     def run(self, state: State) -> Literal[*module_config.DOWNSTREAM_ROUTES]:
         input_messages = [
-            {"role": "system", "content": self.config['prompts']['system_prompt']},
+            {"role": "system", "content": self.system_prompt},
             *state.messages[:-1],
             {"role": "user", "content": f"Route based on this message: {state.messages[-1]['content']}"}
         ]
