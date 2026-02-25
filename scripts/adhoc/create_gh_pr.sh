b0a9a2a43fec853c964c0796936b5b4926f5cd9a
Abhishek Bhatia <bhatiaabhishek8893@gmail.com>
2026-02-25 23:14:22 +0530

    default PR summary file to .local/{branch}/PR_SUMMARY.md

--- scripts/adhoc/create_gh_pr.sh
+++ scripts/adhoc/create_gh_pr.sh
@@ -1,4 +1,5 @@
-FILE=$1
+# Defaults to .local/{branch_name}/PR_SUMMARY.md
+FILE=${1:-.local/$(git branch --show-current)/PR_SUMMARY.md}
 
 TITLE=$(sed -n '/<TITLE>/,/<\/TITLE>/p' "$FILE" | sed '1d;$d')
 BODY=$(sed -n '/<BODY>/,/<\/BODY>/p' "$FILE" | sed '1d;$d')
