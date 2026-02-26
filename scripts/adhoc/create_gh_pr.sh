eb632d9e0e0acc10795179cc6761e56f317b9615
Abhishek Bhatia <bhatiaabhishek8893@gmail.com>
2026-02-26 15:40:03 +0530

    fix create_gr_pr.sh

--- scripts/adhoc/create_gh_pr.sh
+++ scripts/adhoc/create_gh_pr.sh
@@ -1,8 +1,8 @@
 # Defaults to .local/{branch_name}/PR_SUMMARY.md
 FILE=${1:-.local/$(git branch --show-current)/PR_SUMMARY.md}
 
-TITLE=$(sed -n '/<TITLE>/,/<\/TITLE>/p' "$FILE" | sed '1d;$d')
-BODY=$(sed -n '/<BODY>/,/<\/BODY>/p' "$FILE" | sed '1d;$d')
+TITLE=$(sed -n -e '/<TITLE>/,/<\/TITLE>/p' -e '/<\/TITLE>/q' "$FILE" | sed '1d;$d')
+BODY=$(sed -n -e '/<BODY>/,/<\/BODY>/p' -e '/<\/BODY>/q' "$FILE" | sed '1d;$d')
 
 echo "Title: $TITLE"
 echo "Body: $BODY"
