ab47f47a1f3c63e7c6b548ef386bddcd887898c7
Abhishek Bhatia <bhatiaabhishek8893@gmail.com>
2026-05-26 22:59:56 +0530

    revert core/database.py to original state

--- services/ecom-backend/core/database.py
+++ services/ecom-backend/core/database.py
@@ -1,4 +1,3 @@
-import os
 from collections.abc import Generator
 
 from sqlalchemy import create_engine
@@ -7,12 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 from core.config import settings
 
-# CI doesn't play well with relative path
-data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data"))
-os.makedirs(data_dir, exist_ok=True)
-db_file = os.path.join(data_dir, f"{settings.db_name}.db")
-DATABASE_URL = f"sqlite:///{db_file}"
-
+DATABASE_URL = f"sqlite:///data/{settings.db_name}.db"
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
