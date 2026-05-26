2500c2f0b40441b2f30b0d186d71dbf404bf1d13
Abhishek Bhatia <bhatiaabhishek8893@gmail.com>
2026-05-26 22:43:42 +0530

    fix: change to absolute path in core/database.py for DATABASE_URL since CI is acting up

--- services/ecom-backend/core/database.py
+++ services/ecom-backend/core/database.py
@@ -1,3 +1,4 @@
+import os
 from collections.abc import Generator
 
 from sqlalchemy import create_engine
@@ -6,7 +7,12 @@ from sqlalchemy.orm import sessionmaker
 
 from core.config import settings
 
-DATABASE_URL = f"sqlite:///data/{settings.db_name}.db"
+# CI doesn't play well with relative path
+data_dir = os.path.join(os.path.dirname(__file__), "..", "data")
+os.makedirs(data_dir, exist_ok=True)
+db_file = os.path.join(data_dir, f"{settings.db_name}.db")
+DATABASE_URL = f"sqlite:///{db_file}"
+
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
